92f9a6efd5ab2c767f2ec8485229053c72aa8a60
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-07-25 16:31:33 +0200

    feat : Add Tp next sequence with pom completion

---
+++ src/main/java/dev/fr/diginamic/App.java
@@ -0,0 +1,15 @@
+package dev.fr.diginamic;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static  final  Logger LOGGER =LoggerFactory.getLogger( App.class);
+
+    public static void main(String[] args) {
+     LOGGER.info("Bonjour !");
+    }
+}
